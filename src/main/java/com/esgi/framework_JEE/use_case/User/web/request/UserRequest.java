653b4a068de8dbd43ad2049c9eb41e56a45edee5
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-04-13 22:52:59 +0200

    :ambulance:

--- src/main/java/com/esgi/framework_JEE/use_case/User/web/request/UserRequest.java
+++ src/main/java/com/esgi/framework_JEE/use_case/User/web/request/UserRequest.java
@@ -3,10 +3,8 @@ package com.esgi.framework_JEE.use_case.User.web.request;
 
 
 public class UserRequest {
-
     public String firstname;
     public String lastname;
     public String email;
     public String password;
-    public String profilePicture;
 }
