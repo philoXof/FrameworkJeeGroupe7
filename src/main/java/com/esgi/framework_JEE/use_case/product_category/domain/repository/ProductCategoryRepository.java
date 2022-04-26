59d56e71cd60c46d5abc519fa8d73acfc34ad701
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-04-26 16:06:37 +0200

    :rocket: productCategory repository

---
+++ src/main/java/com/esgi/framework_JEE/use_case/product_category/domain/repository/ProductCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.esgi.framework_JEE.use_case.product_category.domain.repository;
+
+import com.esgi.framework_JEE.use_case.product_category.domain.entities.entities.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
+
+    ProductCategory findById(int id);
+}
