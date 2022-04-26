6c85ef6fc350389d42658ac296a9c271dc48b579
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-04-26 15:00:55 +0200

    :art: move package

--- src/main/java/com/esgi/framework_JEE/use_case/product_category/domain/entities/entities/ProductCategory.java
+++ src/main/java/com/esgi/framework_JEE/use_case/product_category/domain/entities/entities/ProductCategory.java
@@ -1,4 +1,4 @@
-package com.esgi.framework_JEE.use_case.product_category.entities;
+package com.esgi.framework_JEE.use_case.product_category.domain.entities.entities;
 
 import lombok.Getter;
 import lombok.Setter;
