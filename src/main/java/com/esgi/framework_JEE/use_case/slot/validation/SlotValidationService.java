cfd3d470b0e8230636ce0ea18082a66c9a20121f
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-04-27 01:24:18 +0200

    :art: slot validation

--- src/main/java/com/esgi/framework_JEE/use_case/slot/validation/SlotValidationService.java
+++ src/main/java/com/esgi/framework_JEE/use_case/slot/validation/SlotValidationService.java
@@ -1,4 +1,12 @@
 package com.esgi.framework_JEE.use_case.slot.validation;
 
+import com.esgi.framework_JEE.use_case.slot.domain.entities.Slot;
+
 public class SlotValidationService {
+
+    public boolean isValid(Slot slot){
+        var end = slot.getEnd();
+        var start = slot.getEnd();
+        return end != null && start != null && !end.before(start);
+    }
 }
