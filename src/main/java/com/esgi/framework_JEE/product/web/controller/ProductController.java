4ead59140aeb970a0f9347c5c457e828831df7a5
xof <ljehanno@myges.fr>
2022-06-12 22:06:32 +0200

    :art: add missing import

--- src/main/java/com/esgi/framework_JEE/product/web/controller/ProductController.java
+++ src/main/java/com/esgi/framework_JEE/product/web/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.esgi.framework_JEE.product.web.request.ProductRequest;
 import com.esgi.framework_JEE.product.web.command.ProductCommand;
 import com.esgi.framework_JEE.product.domain.entities.Product;
 import com.esgi.framework_JEE.product.web.query.ProductQuery;
+import com.esgi.framework_JEE.product_category.domain.entities.ProductCategory;
 import com.esgi.framework_JEE.product_category.query.ProductCategoryQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
