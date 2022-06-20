07c5510266191c19aaa0a89ce0283a6e6bfdd0c3
xof <ljehanno@myges.fr>
2022-06-21 00:45:39 +0200

    :white_check_mark: add token to user tests controller

--- src/test/java/com/esgi/framework_JEE/user/web/controller/UserFixture.java
+++ src/test/java/com/esgi/framework_JEE/user/web/controller/UserFixture.java
@@ -1,5 +1,6 @@
 package com.esgi.framework_JEE.user.web.controller;
 
+import com.esgi.framework_JEE.Token;
 import com.esgi.framework_JEE.user.web.request.UserRequest;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -15,71 +16,78 @@ public class UserFixture {
                 .post("/user/create");
     }
 
-    public static Response getById(int userId){
+    public static Response getById(int userId, Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
+                .header("Authorization","Bearer "+token.access_token)
                 .get("/user/" + userId);
     }
 
 
 
-    public static Response changeLastname(int userId, UserRequest userRequest){
+    public static Response changeLastname(int userId, UserRequest userRequest, Token token){
 
         return given()
                 .contentType(ContentType.JSON)
                 .when()
                 .body(userRequest)
+                .header("Authorization","Bearer "+token.access_token)
                 .patch("/user/lastname/" + userId);
     }
 
-    public static Response changeFirstname(int userId, UserRequest userRequest){
+    public static Response changeFirstname(int userId, UserRequest userRequest, Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
                 .body(userRequest)
+                .header("Authorization","Bearer "+token.access_token)
                 .patch("/user/firstname/" + userId);
     }
-    public static Response changePassword(int userId, UserRequest userRequest){
+    public static Response changePassword(int userId, UserRequest userRequest, Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
                 .body(userRequest)
+                .header("Authorization","Bearer "+token.access_token)
                 .patch("/user/password/" + userId);
     }
 
-    public static Response changeEmail(int userId, UserRequest userRequest){
+    public static Response changeEmail(int userId, UserRequest userRequest, Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
                 .body(userRequest)
+                .header("Authorization","Bearer "+token.access_token)
                 .patch("/user/email/" + userId);
 
     }
 
 
-    public static Response deleteById(int userId){
+    public static Response deleteById(int userId, Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
+                .header("Authorization","Bearer "+token.access_token)
                 .delete("/user/" + userId);
     }
 
-    public static Response getAll(){
+    public static Response getAll(Token token){
         return given()
                 .contentType(ContentType.JSON)
                 .when()
+                .header("Authorization","Bearer "+token.access_token)
                 .get("/user/");
     }
 
-
-    public static Response login(UserRequest userRequest){
-        return given()
-                .contentType(ContentType.JSON)
-                .when()
-                .body(userRequest)
-                .get("/user/login");
-    }
+//
+//    public static Response login(UserRequest userRequest){
+//        return given()
+//                .contentType(ContentType.JSON)
+//                .when()
+//                .body(userRequest)
+//                .get("/user/login");
+//    }
 
 }
 
